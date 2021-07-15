a0cdac9f07529eb938c3842f48b39c4dc3b8811d
IVAN <1960867034@qq.com>
2021-07-15 16:30:36 +0800

    push test commit

--- src/main/java/com/guocz2/git/GitTest.java
+++ src/main/java/com/guocz2/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello world3");
         System.out.println("hello world4");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
