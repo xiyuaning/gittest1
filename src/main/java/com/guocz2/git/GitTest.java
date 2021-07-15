7044902e872e507d8d68ac5f9bbdc73645abed78
IVAN <1960867034@qq.com>
2021-07-15 15:29:59 +0800

    first commit

---
+++ src/main/java/com/guocz2/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.guocz2.git;
+
+/**
+ * Created by gcz on 2021/7/15 15:26.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
