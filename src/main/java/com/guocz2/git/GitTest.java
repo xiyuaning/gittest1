06ac64a3321b03b36e3ffdc634035d9deea9df4a
IVAN <1960867034@qq.com>
2021-07-15 15:54:59 +0800

    hot-fix commit2

--- src/main/java/com/guocz2/git/GitTest.java
+++ src/main/java/com/guocz2/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world2");
         System.out.println("hello world3");
+        System.out.println("hot-fix test");
     }
 }
