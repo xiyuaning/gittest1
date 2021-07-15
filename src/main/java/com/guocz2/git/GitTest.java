1cd8e73c4c0c2bb7acaee86cbde0a3ea72a8724f
IVAN <1960867034@qq.com>
2021-07-15 15:32:27 +0800

    second commit

--- src/main/java/com/guocz2/git/GitTest.java
+++ src/main/java/com/guocz2/git/GitTest.java
@@ -6,5 +6,6 @@ package com.guocz2.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
